7db2d1df5837011204c6428c38545ae5e81144d0
seongjun <skim477@myseneca.ca>
2025-02-20 19:15:42 -0500

    setup frontend

---
+++ teamworks-frontend/src/components/TestComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TestComponent() {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="text-primary">Hello, Bootstrap!</h1>
+      <p>This is a test component using Bootstrap styles.</p>
+      <button className="btn btn-success">Click Me</button>
+    </div>
+  );
+}
+
+export default TestComponent;
---
+++ teamworks-frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TestComponent from '../components/TestComponent';
+function HomePage() {
+  return (
+    <div className="container mt-5">
+      <h2>Home Page</h2>
+      <p>Welcome to the Home Page!</p>
+      < TestComponent />
+    </div>
+  );
+}
+
+export default HomePage;
