ce96e7e92b92b6ab1b827f474b1dbdb994c70b92
Bagglebob <fawad-arshad96@hotmail.com>
2025-10-10 18:59:28 -0400

    Sprint 2 front end

--- teamworks-frontend/src/pages/Home.js
+++ teamworks-frontend/src/pages/Home.js
@@ -1,26 +1,52 @@
 import React, { useState, useEffect } from 'react';
+import { AuthProvider, useAuth } from '../contexts/AuthContext';
+import axios from "axios";
+import { Button } from 'bootstrap/dist/js/bootstrap.bundle.min';
 
-function HomePage() {
-  const [message, setMessage] = useState('');
+
+
+
+function HomePage({ projs, user}) {
+  const [projects, setProjects] = useState(projs || []);
+
+  const handleDelete = async (projectId, user) => {
+    try {
+      await axios.delete(`${process.env.REACT_APP_API_URL}/api/projects/${projectId}`);
+      alert("Project deleted");
+      setProjects(projects.filter(proj => proj.id !== projectId)); // update UI
+
+      // optionally update state to remove deleted project from UI
+    } catch (err) {
+      console.error("Failed to delete project", err);
+      alert("Failed to delete project");
+    }
+  }
 
   useEffect(() => {
-    const fetchHomeMessage = async () => {
+    const fetchProjects = async () => {
       try {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/`);
-        const data = await response.json();
-        setMessage(data.message);
-      } catch (error) {
-        console.error('Error fetching data:', error);
+        const res = await axios.get(
+          `${process.env.REACT_APP_API_URL}/api/projects/${user.id}`
+        );
+        console.log(res.data)
+        setProjects(res.data);
+      } catch (err) {
+        console.error("Failed to fetch projects", err);
       }
-    };
+    }
 
-    fetchHomeMessage();
-  }, []);
+    fetchProjects();
+  }, [user]);
 
   return (
     <div className="container mt-4">
       <h1 className="text-center">Home Page</h1>
-      <p className="text-center">{message}</p>
+      {projects.map(project => (
+        <div key={project.id}>
+          {project.name}
+          <button onClick={() => handleDelete(project.id)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
 }
